cd0aaee48efa4a40cbe95582a5da851cd50f12d6
Evgeniy Vasiliev <vasilievdevelop@gmail.com>
2023-08-13 14:25:45 +0300

    remove btn show form

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-const presentOrderBtn = document.querySelector('.present__order-btn'),
-      pageOverlayMmodal = document.querySelector('.page__overlay_modal'),
-      modalClose = document.querySelector('.modal__close');
+// const presentOrderBtn = document.querySelector('.present__order-btn'),
+//       pageOverlayMmodal = document.querySelector('.page__overlay_modal'),
+//       modalClose = document.querySelector('.modal__close');
 
-const hendlerModal = (openBtn, modal, openSelector, closeTrigger) => {
-  openBtn.addEventListener('click', () => {
-    modal.classList.add(openSelector);
-  });
+// const hendlerModal = (openBtn, modal, openSelector, closeTrigger) => {
+//   openBtn.addEventListener('click', () => {
+//     modal.classList.add(openSelector);
+//   });
 
-  closeTrigger.addEventListener('click', () => {
-    modal.classList.remove(openSelector);
-  });
-};
+//   closeTrigger.addEventListener('click', () => {
+//     modal.classList.remove(openSelector);
+//   });
+// };
 
-hendlerModal(presentOrderBtn, pageOverlayMmodal, 'page__overlay_modal_open', modalClose);
+// hendlerModal(presentOrderBtn, pageOverlayMmodal, 'page__overlay_modal_open', modalClose);
